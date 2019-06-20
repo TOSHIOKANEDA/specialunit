030338c1cca73a514f6c26a0b7ec4542b0cc0b32
EC2 Default User <ec2-user@ip-172-31-9-168.ap-southeast-1.compute.internal>
2019-06-20 17:10:50 +0000

    mailer x 3とdev x 1, login周り直したぜ

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,15 +1,5 @@
 class InquiryMailer < ApplicationMailer
   
-  # fromはuser.admin:trueのemailとcurrent_user.email, 追加で送って欲しい人に送るように設定
-  
-  def send_when_update(booking)
-    comment = 'Specialunitの在庫問い合わせきますた'
-    @booking = booking
-    mail(
-      from: 'camp1of1tech@gmail.com',
-      to:   'camp1of1tech@gmail.com',
-      subject: comment && booking.email
-    )
-  end
+ 
   
 end
