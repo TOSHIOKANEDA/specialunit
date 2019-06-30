2789f2794778850752cd73b23905a76ed89cf40d
EC2 Default User <ec2-user@ip-172-31-9-168.ap-southeast-1.compute.internal>
2019-06-30 17:34:45 +0000

    css調整なう

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
    address:              'smtp.gmail.com',
    port:                  587,
    domain:               'gmail.com',
-   user_name:            'camp1of1tech@gmail.com',
-   password:             'egtiirnaznljogoq',
+   user_name:            '必ずrails c@gmail.com',
+   password:             '必ずrails c',
    authentication:       'plain',
    enable_starttls_auto:  true
   }
