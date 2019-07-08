9a00d4259d31673455b4657bd65dc294d9084196
EC2 Default User <ec2-user@ip-172-31-26-29.ap-southeast-1.compute.internal>
2019-07-08 11:01:51 +0000

    mailer5

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'camp1of1tech@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'whispering-tor-36194.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-   address:              'smtp.gmail.com',
-   port:                  587,
-   domain:               'gmail.com',
-   user_name:            'camp1of1tech@gmail.com',
-   password:             'egtiirnaznljogoq',
-   authentication:       'plain',
-   enable_starttls_auto:  true
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :domain => 'smtp.gmail.com',
+      :user_name => 'camp1of1tech@gmail.com',
+      :password => 'egtiirnaznljogoq',
+      :authentication => :plain,
+      :enable_starttls_auto => true
   }
   
 end
